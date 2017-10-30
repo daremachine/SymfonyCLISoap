3e9f8dbb2dca2a09089bc8353c2f96659d294097
Giacommo Hantak <d4red3vill@gmail.com>
2017-10-30 21:36:44 +0100

    add command

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 use Symfony\Component\Console\Application;
+use UkLocation\Commands\GetUKLocationByTownCommand;
 
 $app = new Application();
+$app->add(new GetUKLocationByTownCommand());
 $app->run();
