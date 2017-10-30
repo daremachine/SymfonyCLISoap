b9ba37756d7c57811aa46ce831cd1fef162e1dc0
Giacommo Hantak <d4red3vill@gmail.com>
2017-10-30 22:00:58 +0100

    add exceptions

---
+++ src/UkLocation/Exceptions/WsdlNotLoadException.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace UkLocation\Exceptions;
+
+class WsdlNotLoadException extends \RuntimeException implements IException
+{
+    public function __construct($message = "", $code = 0, Exception $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}
