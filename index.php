1d58e39328b5bd2597c74a19173aefd3f73ce199
Giacommo Hantak <d4red3vill@gmail.com>
2017-10-30 21:44:59 +0100

    rename

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 use Symfony\Component\Console\Application;
-use UkLocation\Commands\GetUKLocationByTownCommand;
+use UkLocation\Commands\GetUKLocationByTownsCommand;
 
 $app = new Application();
-$app->add(new GetUKLocationByTownCommand());
+$app->add(new GetUKLocationByTownsCommand());
 $app->run();
