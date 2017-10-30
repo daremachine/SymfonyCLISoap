ea2fa2458c51ee138aaa39f2e31aedf9eeef9164
Giacommo Hantak <d4red3vill@gmail.com>
2017-10-30 22:48:03 +0100

    add tests for UkLocation

--- tests/UkLocationTest.php
+++ tests/UkLocationTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace Tests;
 
 use PHPUnit\Framework\TestCase;
+use UkLocation\Exceptions\BadCountParametersException;
 use UkLocation\SoapClient;
 
 require_once  './vendor/autoload.php';
@@ -16,6 +17,7 @@ class UkLocationTest extends TestCase
 
     public function testGetUkLocationByTownsCommand()
     {
+        // formby - is town name, a - should generate empty result
         $tester = \Tests\TestBootstrapFactory::create("UkLocation:GetUKLocationByTowns", [
             'Towns' => ['formby', 'failingName0']
         ]);
@@ -23,16 +25,46 @@ class UkLocationTest extends TestCase
         $result = $tester->getDisplay();
 
         $this->assertEquals(is_string($result), true);
+        $this->assertNotEmpty($result);
+        $this->assertContains('PostCode:', $result);
+        $this->assertContains('No results found', $result);
     }
 
     public function testGetUkLocationByTownVerifyData()
     {
-        $towns = ['formby', 'b'];
-        $soapClient = new SoapClient('http://www.webservicex.net/uklocation.asmx?WSDL');
+        $towns = ['formby', 'failingName0'];
+        $soapClient = new SoapClient(self::TEST_WSDL_REMOTE);
         $data = $soapClient->GetUkLocationByTown($towns);
 
         $this->assertEquals(count($data->getTownLocations()) > 0, true);
         $this->assertNotEmpty($data->getTownLocations()[0]->getTown());
         $this->assertNotEmpty($data->getTownLocations()[0]->getPostCode());
+    }
+
+    public function testGetLocationWithOneTownShouldFail()
+    {
+        $this->expectException(BadCountParametersException::class);
+
+        $towns = ['failingName0'];
+        $soapClient = new SoapClient(self::TEST_WSDL_REMOTE);
+        $data = $soapClient->GetUkLocationByTown($towns);
+    }
+
+    public function testGetLocationWithMoreThreeTownShouldFail()
+    {
+        $this->expectException(BadCountParametersException::class);
+
+        $towns = ['failingName0', 'failingName0', 'failingName0', 'failingName0'];
+        $soapClient = new SoapClient(self::TEST_WSDL_REMOTE);
+        $data = $soapClient->GetUkLocationByTown($towns);
+    }
+
+    public function testSoapClientConnectionDownShouldFail()
+    {
+        $this->expectException(\Exception::class);
+
+        $towns = ['failingName0', 'failingName0', 'failingName0'];
+        $soapClient = new SoapClient(self::TEST_WSDL_REMOTE_FAIL);
+        $data = $soapClient->GetUkLocationByTown($towns);
     }
 }
